86fc4c2a564039691975333126212a11dbee7e48
Yenifer <guajeyenifer@gmail.com>
2021-12-07 21:41:31 -0500

    Se agrega el test que valida el titulo y el contenido basico

--- src/functions.test.js
+++ src/functions.test.js
@@ -1,7 +1,39 @@
 import { createEvent } from './functions'
 
+beforeAll(() => {
+    global.Date.now = jest.fn(() => new Date('2021-12-07T10:20:30Z').getTime())
+})
+
+const NUM_DAY = { 'mon': 1, 'tue': 2, 'wed': 3, 'thu': 4, 'fri': 5, 'sat': 6, 'sun': 7 };
+
+const weekday = "mon";
+const week = 1;
+const openHour=8;
+const closeHour=14
+
+const numDay = NUM_DAY[weekday];
+    const currentDay = new Date().getDay();
+    const hour = new Date().getHours();
+    const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };   
+
+    function addDays(days) {
+        return new Date(new Date().setDate(new Date().getDate() + days));
+    }
+
+    function getDateCalendar(numDay, currentDay) {
+        if (numDay >= currentDay && parseInt(closeHour) >= hour) {//posterior a dia de la semana
+            return addDays((numDay - currentDay) + 7 * (week - 1));
+        }
+        return addDays((numDay - currentDay) + 7 * (week - 1));
+    }
+
 test('Validation a event title and content basic', () => {
     //TODO: hacer las verificaciones
+    const result = createEvent(weekday, week, openHour, closeHour);
+
+    expect(result.title).toBe("[SOFKA U] Meeting Room");
+    expect(result.description).toBe("Mentoring and Practice");
+    expect(result.duration).toEqual([6, 'hour']);
 });
 
 test('Validation start date', () => {
